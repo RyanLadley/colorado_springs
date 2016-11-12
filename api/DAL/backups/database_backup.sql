424e561f9d89344783011b3ad27f3d5b99ef4100
Ryan Ladley <ladley.ryan@gmail.com>
2016-11-11 19:08:36 -0700

    DB Backup added

--- api/DAL/backups/database_backup.sql
+++ api/DAL/backups/database_backup.sql
@@ -50,6 +50,31 @@ INSERT INTO `accounts` VALUES (1,5221000,NULL,NULL,'In House Resurfacing',216093
 UNLOCK TABLES;
 
 --
+-- Table structure for table `transaction_types`
+--
+
+DROP TABLE IF EXISTS `transaction_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction_types` (
+  `transaction_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `transaction_type` varchar(45) NOT NULL,
+  PRIMARY KEY (`transaction_type_id`),
+  UNIQUE KEY `transaction_type_id_UNIQUE` (`transaction_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction_types`
+--
+
+LOCK TABLES `transaction_types` WRITE;
+/*!40000 ALTER TABLE `transaction_types` DISABLE KEYS */;
+INSERT INTO `transaction_types` VALUES (1,'Equipment'),(2,'Labor'),(3,'Material');
+/*!40000 ALTER TABLE `transaction_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `transactions`
 --
 
@@ -58,16 +83,18 @@ DROP TABLE IF EXISTS `transactions`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transactions` (
   `transaction_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` int(10) unsigned NOT NULL,
   `vendor_id` int(10) unsigned NOT NULL,
   `invoice_date` date NOT NULL,
   `date_paid` date DEFAULT NULL,
-  `invoice_number` varchar(140) NOT NULL,
+  `invoice_no` varchar(140) NOT NULL,
   `description` varchar(360) NOT NULL,
   `expense` varchar(360) NOT NULL,
+  `transaction_type_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`transaction_id`),
   UNIQUE KEY `transaction_id_UNIQUE` (`transaction_id`),
-  UNIQUE KEY `vendor_invoice_UNIQUE` (`invoice_number`,`vendor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  UNIQUE KEY `vendor_invoice_UNIQUE` (`invoice_no`,`vendor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,6 +103,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,2,3,'2016-11-08','2016-11-15','LP09K87HY','This is the first entry.','69.36',1),(2,6,4,'2016-11-22','2016-11-16','ALB892JSA','Figure out why total expensed pops up with required text when Data button is pressed.','950.52',2),(4,7,6,'2017-01-09','2016-11-23','AHK13GDS','I bought a thing','23',2);
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,15 +117,77 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 /*!50001 CREATE TABLE `v_transactions` (
   `transaction_id` tinyint NOT NULL,
+  `account_id` tinyint NOT NULL,
+  `account_no` tinyint NOT NULL,
+  `sub_no` tinyint NOT NULL,
+  `shred_no` tinyint NOT NULL,
   `vendor_id` tinyint NOT NULL,
   `vendor_name` tinyint NOT NULL,
   `invoice_date` tinyint NOT NULL,
   `date_paid` tinyint NOT NULL,
-  `invoice_number` tinyint NOT NULL,
+  `invoice_no` tinyint NOT NULL,
   `description` tinyint NOT NULL,
-  `expense` tinyint NOT NULL
+  `expense` tinyint NOT NULL,
+  `transaction_type_id` tinyint NOT NULL,
+  `transaction_type` tinyint NOT NULL
 ) ENGINE=MyISAM */;
 SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `v_vendors`
+--
+
+DROP TABLE IF EXISTS `v_vendors`;
+/*!50001 DROP VIEW IF EXISTS `v_vendors`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `v_vendors` (
+  `vendor_id` tinyint NOT NULL,
+  `name` tinyint NOT NULL,
+  `contract_no` tinyint NOT NULL,
+  `contract_start` tinyint NOT NULL,
+  `contract_end` tinyint NOT NULL,
+  `point_of_contact` tinyint NOT NULL,
+  `phone_no` tinyint NOT NULL,
+  `address` tinyint NOT NULL,
+  `city` tinyint NOT NULL,
+  `state` tinyint NOT NULL,
+  `zip` tinyint NOT NULL,
+  `email` tinyint NOT NULL,
+  `website` tinyint NOT NULL,
+  `image_folder` tinyint NOT NULL,
+  `image_file_name` tinyint NOT NULL,
+  `image_file_type` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `vendor_images`
+--
+
+DROP TABLE IF EXISTS `vendor_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vendor_images` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `vendor_id` int(10) unsigned NOT NULL,
+  `folder` varchar(45) DEFAULT NULL,
+  `file_name` varchar(45) DEFAULT NULL,
+  `file_type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vendor_images`
+--
+
+LOCK TABLES `vendor_images` WRITE;
+/*!40000 ALTER TABLE `vendor_images` DISABLE KEYS */;
+INSERT INTO `vendor_images` VALUES (2,3,'0','3','png'),(3,4,'0','4','png'),(4,6,'0','6','jpeg');
+/*!40000 ALTER TABLE `vendor_images` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `vendors`
@@ -109,13 +199,12 @@ DROP TABLE IF EXISTS `vendors`;
 CREATE TABLE `vendors` (
   `vendor_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(140) NOT NULL,
-  `icon_path` varchar(140) DEFAULT NULL,
   `contract_no` varchar(45) NOT NULL,
   `contract_start` date DEFAULT NULL,
   `contract_end` date NOT NULL,
   `point_of_contact` varchar(140) DEFAULT NULL,
-  `phone_no` varchar(10) DEFAULT NULL,
-  `street` varchar(360) DEFAULT NULL,
+  `phone_no` varchar(12) DEFAULT NULL,
+  `address` varchar(360) DEFAULT NULL,
   `city` varchar(360) DEFAULT NULL,
   `state` varchar(360) DEFAULT NULL,
   `zip` varchar(6) DEFAULT NULL,
@@ -123,7 +212,7 @@ CREATE TABLE `vendors` (
   `website` varchar(360) DEFAULT NULL,
   PRIMARY KEY (`vendor_id`),
   UNIQUE KEY `vendor_id_UNIQUE` (`vendor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,6 +221,7 @@ CREATE TABLE `vendors` (
 
 LOCK TABLES `vendors` WRITE;
 /*!40000 ALTER TABLE `vendors` DISABLE KEYS */;
+INSERT INTO `vendors` VALUES (3,'Grainger','TS1534','2015-06-11','2018-03-23','Tom McGrainger','719-555-3924','1234 Fake Street','Colorado Springs','Colorado','80918','tom@grainger.com','www.grainger.com'),(4,'Concrete Co','PL435A','2015-04-06','2017-12-02','Mr Concrete','719-555-63','987 Not Real Road','Peyton','Colorado','80810','mr@concrete.ord','www.google.com/'),(6,'AAA Building Supply','DS09098','2015-04-12','2018-06-02','Lord Vader','702-963-8745','321 No Moon Lane','Seattle','Washington','85634','vader@deathstar.net','www.starwars.com');
 /*!40000 ALTER TABLE `vendors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,7 +239,26 @@ UNLOCK TABLES;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `v_transactions` AS select `t`.`transaction_id` AS `transaction_id`,`t`.`vendor_id` AS `vendor_id`,`vendors`.`name` AS `vendor_name`,`t`.`invoice_date` AS `invoice_date`,`t`.`date_paid` AS `date_paid`,`t`.`invoice_number` AS `invoice_number`,`t`.`description` AS `description`,`t`.`expense` AS `expense` from (`transactions` `t` join `vendors`) where (`t`.`vendor_id` = `vendors`.`vendor_id`) */;
+/*!50001 VIEW `v_transactions` AS select `t`.`transaction_id` AS `transaction_id`,`t`.`account_id` AS `account_id`,`accounts`.`account_no` AS `account_no`,`accounts`.`sub_no` AS `sub_no`,`accounts`.`shred_no` AS `shred_no`,`t`.`vendor_id` AS `vendor_id`,`vendors`.`name` AS `vendor_name`,`t`.`invoice_date` AS `invoice_date`,`t`.`date_paid` AS `date_paid`,`t`.`invoice_no` AS `invoice_no`,`t`.`description` AS `description`,`t`.`expense` AS `expense`,`t`.`transaction_type_id` AS `transaction_type_id`,`transaction_types`.`transaction_type` AS `transaction_type` from (((`transactions` `t` join `vendors` on((`t`.`vendor_id` = `vendors`.`vendor_id`))) join `transaction_types` on((`transaction_types`.`transaction_type_id` = `t`.`transaction_type_id`))) join `accounts` on((`accounts`.`account_id` = `t`.`account_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `v_vendors`
+--
+
+/*!50001 DROP TABLE IF EXISTS `v_vendors`*/;
+/*!50001 DROP VIEW IF EXISTS `v_vendors`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `v_vendors` AS select `vendors`.`vendor_id` AS `vendor_id`,`vendors`.`name` AS `name`,`vendors`.`contract_no` AS `contract_no`,`vendors`.`contract_start` AS `contract_start`,`vendors`.`contract_end` AS `contract_end`,`vendors`.`point_of_contact` AS `point_of_contact`,`vendors`.`phone_no` AS `phone_no`,`vendors`.`address` AS `address`,`vendors`.`city` AS `city`,`vendors`.`state` AS `state`,`vendors`.`zip` AS `zip`,`vendors`.`email` AS `email`,`vendors`.`website` AS `website`,`image`.`folder` AS `image_folder`,`image`.`file_name` AS `image_file_name`,`image`.`file_type` AS `image_file_type` from (`vendors` join `vendor_images` `image` on((`image`.`vendor_id` = `vendors`.`vendor_id`))) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -163,4 +272,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-06 11:51:25
+-- Dump completed on 2016-11-11 19:07:38
