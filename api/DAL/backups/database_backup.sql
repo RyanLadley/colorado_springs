c7bf57ea46817047fb26579c4d68db589882d4be
RyanLadley <ladley.ryann@gmail.com>
2016-11-14 22:51:35 -0700

    DB Backup added

--- api/DAL/backups/database_backup.sql
+++ api/DAL/backups/database_backup.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `colorado_springs` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `colorado_springs`;
--- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.52, for debian-linux-gnu (x86_64)
 --
 -- Host: 127.0.0.1    Database: colorado_springs
 -- ------------------------------------------------------
--- Server version	5.5.49-0ubuntu0.14.04.1
+-- Server version	5.5.52-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,15 +25,15 @@ DROP TABLE IF EXISTS `account_transfers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account_transfers` (
-  `transfer` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `transfer_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `from_account_id` int(10) unsigned NOT NULL,
   `to_account_id` int(10) unsigned NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `description` varchar(360) NOT NULL,
   `transfer_date` date NOT NULL,
-  PRIMARY KEY (`transfer`),
-  UNIQUE KEY `trasnfer_id_UNIQUE` (`transfer`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`transfer_id`),
+  UNIQUE KEY `trasnfer_id_UNIQUE` (`transfer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `account_transfers` (
 
 LOCK TABLES `account_transfers` WRITE;
 /*!40000 ALTER TABLE `account_transfers` DISABLE KEYS */;
-INSERT INTO `account_transfers` VALUES (2,2,15,53.60,'This is a test','2016-11-14');
+INSERT INTO `account_transfers` VALUES (2,2,15,53.60,'This is a test','2016-11-14'),(3,1,14,23.00,'Another test','2016-11-14');
 /*!40000 ALTER TABLE `account_transfers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,6 @@ CREATE TABLE `accounts` (
   `shred_no` int(2) DEFAULT NULL,
   `description` varchar(140) NOT NULL,
   `annual_budget` decimal(10,2) NOT NULL,
-  `transfer` decimal(10,2) NOT NULL,
   PRIMARY KEY (`account_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -71,7 +70,7 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (1,5221000,NULL,NULL,'In House Resurfacing',2160938.00,0.39),(2,5221000,1,NULL,'Maitenance Paving',0.00,0.00),(4,5221000,2,NULL,'Structural Digout',0.00,0.00),(5,5221000,2,1,'North District Digout',0.00,0.00),(6,5221000,2,2,'South District Digout',0.00,0.00),(7,5221000,2,3,'East District Digout',0.00,0.00),(8,5221000,2,4,'West District Digout',0.00,0.00),(9,5221000,3,NULL,'District Maintenance Paving',0.00,0.00),(10,5221000,3,1,'North Maintenence Paving',0.00,0.00),(11,5221000,3,2,'South Maintenance Paving',0.00,0.00),(12,5221000,3,3,'East Maintenance Paving',0.00,0.00),(13,5221000,3,4,'West Maintenance Paving',0.00,0.00),(14,5222000,NULL,NULL,'Pothole Patching Repair',0.00,0.00),(15,5222000,1,NULL,'Asphalt Repair',0.00,0.00),(16,5222000,2,NULL,'Propane',0.00,0.00),(17,5222000,3,NULL,'Shovels/Rakes/etc',0.00,0.00);
+INSERT INTO `accounts` VALUES (1,5221000,NULL,NULL,'In House Resurfacing',2160938.00),(2,5221000,1,NULL,'Maitenance Paving',0.00),(4,5221000,2,NULL,'Structural Digout',0.00),(5,5221000,2,1,'North District Digout',0.00),(6,5221000,2,2,'South District Digout',0.00),(7,5221000,2,3,'East District Digout',0.00),(8,5221000,2,4,'West District Digout',0.00),(9,5221000,3,NULL,'District Maintenance Paving',0.00),(10,5221000,3,1,'North Maintenence Paving',0.00),(11,5221000,3,2,'South Maintenance Paving',0.00),(12,5221000,3,3,'East Maintenance Paving',0.00),(13,5221000,3,4,'West Maintenance Paving',0.00),(14,5222000,NULL,NULL,'Pothole Patching Repair',0.00),(15,5222000,1,NULL,'Asphalt Repair',0.00),(16,5222000,2,NULL,'Propane',0.00),(17,5222000,3,NULL,'Shovels/Rakes/etc',0.00);
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +119,7 @@ CREATE TABLE `transactions` (
   PRIMARY KEY (`transaction_id`),
   UNIQUE KEY `transaction_id_UNIQUE` (`transaction_id`),
   UNIQUE KEY `vendor_invoice_UNIQUE` (`invoice_no`,`vendor_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,9 +128,33 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` VALUES (1,2,3,'2016-11-08','2016-11-15','LP09K87HY','This is the first entry.','69.36',1),(2,6,4,'2016-11-22','2016-11-16','ALB892JSA','Figure out why total expensed pops up with required text when Data button is pressed.','950.52',2),(4,7,6,'2017-01-09','2016-11-23','AHK13GDS','This is an updated transaction','23',2),(5,4,6,'2016-11-16','2016-11-15','KJHSAK','This is a pending transaction.At least, it was','12.43',1);
+INSERT INTO `transactions` VALUES (1,2,3,'2016-11-08','2016-11-15','LP09K87HY','This is the first entry.','69.36',1),(2,6,4,'2016-11-22','2016-11-16','ALB892JSA','There is no way to clear the \"date paid\" on the transaction adjustment screen. This would be necessary if a user would want to change a transaction to pending.','950.52',2),(4,7,6,'2017-01-09','2016-11-23','AHK13GDS','This is an updated transaction','23',2),(5,4,6,'2016-11-16','2016-11-15','KJHSAK','This is a pending transaction.At least, it was','12.43',1),(6,16,3,'2016-11-15','2016-11-17','JS-12-SJ','There needs to a character counter on the description field since te database has a character limit.','12.42',1);
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `v_account_transfers`
+--
+
+DROP TABLE IF EXISTS `v_account_transfers`;
+/*!50001 DROP VIEW IF EXISTS `v_account_transfers`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `v_account_transfers` (
+  `transfer_id` tinyint NOT NULL,
+  `from_account_id` tinyint NOT NULL,
+  `from_account_no` tinyint NOT NULL,
+  `from_sub_no` tinyint NOT NULL,
+  `from_shred_no` tinyint NOT NULL,
+  `to_account_id` tinyint NOT NULL,
+  `to_account_no` tinyint NOT NULL,
+  `to_sub_no` tinyint NOT NULL,
+  `to_shred_no` tinyint NOT NULL,
+  `amount` tinyint NOT NULL,
+  `description` tinyint NOT NULL,
+  `transfer_date` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Temporary table structure for view `v_transactions`
@@ -252,6 +275,25 @@ INSERT INTO `vendors` VALUES (3,'Grainger','TS1534','2015-06-11','2018-03-23','T
 UNLOCK TABLES;
 
 --
+-- Final view structure for view `v_account_transfers`
+--
+
+/*!50001 DROP TABLE IF EXISTS `v_account_transfers`*/;
+/*!50001 DROP VIEW IF EXISTS `v_account_transfers`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `v_account_transfers` AS select `account_transfers`.`transfer_id` AS `transfer_id`,`account_transfers`.`from_account_id` AS `from_account_id`,`from_account`.`account_no` AS `from_account_no`,`from_account`.`sub_no` AS `from_sub_no`,`from_account`.`shred_no` AS `from_shred_no`,`account_transfers`.`to_account_id` AS `to_account_id`,`to_account`.`account_no` AS `to_account_no`,`to_account`.`sub_no` AS `to_sub_no`,`to_account`.`shred_no` AS `to_shred_no`,`account_transfers`.`amount` AS `amount`,`account_transfers`.`description` AS `description`,`account_transfers`.`transfer_date` AS `transfer_date` from ((`account_transfers` join `accounts` `from_account` on((`from_account`.`account_id` = `account_transfers`.`from_account_id`))) join `accounts` `to_account` on((`to_account`.`account_id` = `account_transfers`.`to_account_id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
 -- Final view structure for view `v_transactions`
 --
 
@@ -298,4 +340,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-14 16:15:45
+-- Dump completed on 2016-11-14 22:51:06
